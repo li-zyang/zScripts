9946c9c0157205bb1c9825e97047a52e3fe5e7b8
li-zhaoyang <K_AEIx@163.com>
2020-04-06 09:29:17 +0800

    Good-For-Reading: Fix the html width (set to min 100vw); Added inclusions; Modified rules

--- Good-For-Reading/Good-For-Reading.user.js
+++ Good-For-Reading/Good-For-Reading.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   旧式网页阅读优化
 // @name:zh-TW   舊式網頁閲讀優化
 // @namespace    https://github.com/li-zyang/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Limit the width of old-styled websites (which has it's text content expand across the whole wide window.GoodForReading) and add a catalogue sidebar for a comfortble reading. This script does not affect those "modern" web pages (At least I tried to avoid that. If it fails, you can report the bug on github).
 // @description:zh-CN 调整旧式网页（指那些几乎没有排版，直接用 <h1>、<p> 等等标签从上到下堆下来的网页）的排版，将内容宽度限制为适合阅读的宽度，添加目录侧栏，点击可以跳转到对应位置。此脚本不影响“现代化的”网页（至少我写的时候是这么想的，如果有误伤，可以到 github 上告诉我）。
 // @description:zh-TW 調整舊式網頁（指那些幾乎沒有排版，直接用 <h1>、<p> 等等標簽從上到下堆下來的網頁）的排版，將内容寬度限制為適合閲讀的寬度，添加目錄側欄，點擊可以跳轉到對應位置。此脚本不影響“現代化的”網頁（至少我寫的時候是這麽想的，如果有誤傷，可以到 github 上告訴我）。
@@ -17,9 +17,11 @@
 // @note  v1.0.1      2020-03-12  Fixed the bug problem of jquery conflict which makes some websites fails to load (#1)
 // @note  v1.0.2      2020-03-16  Ignored the media viewer(e.g. **.mp4) & special pages (contains only one element)
 // @note  v1.0.3      2020-03-23  Submit varibles to the real window.GoodForReading to better handling interactions
+// @note  v1.0.4      2020-04-06  Fix the html width (set to min 100vw); Added inclusions; Modified rules
 // ==/UserScript==
 
 // example: https://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
+// example: https://man.openbsd.org/ssh-keygen.1
 
 $.noConflict();
 (function(window, $) {
@@ -29,7 +31,9 @@ $.noConflict();
     /https?:\/\/ssp.scnu.edu.cn\/.*/,
     // 信息化部是真的非主流
     ];
-  let forced_url_pat = [];
+  let forced_url_pat = [
+    /https?:\/\/man.openbsd.org\/.*/
+  ];
   if ($('body *').length <= 1 || $('body *').length == undefined) {
     return 0;
   }
@@ -109,6 +113,7 @@ $.noConflict();
     display: grid;
     grid-template-columns: 100%;
     justify-items: stretch;
+    min-width: 100vw;
   }
   body {
     display: grid;
@@ -188,7 +193,7 @@ $.noConflict();
       $('body > nav').length ||
       $('body > artical').length ||
       $('body > aside').length ||
-      $('body > form').length ||
+      $('body > form:first-child:only-child').length ||
       $('body > video:first-child').length ||
       $('body > img:first-child').length ||
       $('body > svg:first-child').length ||
